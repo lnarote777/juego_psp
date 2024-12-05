4e5e429c8eac1c0877891588f9dfbd29baea7d77
lnarote777 <lnarote777@iesrafaelalberti.es>
2024-12-05 19:32:44 +0100

    player cambios

--- scripts/Wizard.cs
+++ scripts/Wizard.cs
@@ -1,40 +1,65 @@
 using Godot;
-using System;
 
 public partial class Wizard : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	[Export] public float Speed = 100f;
+	private float _maxHealth = 10f;
+	private float _currentHealth;
+	public const float JumpVelocity = -500.0f;
+	private float _acceleration = .25f;
+	private float _friction = .1f;
+	private int _gravity = 4000;
+	private int _jumpHeight = 300;
+	private int _dashSpeed = 1000;
 
-	public override void _PhysicsProcess(double delta)
+
+	AnimatedSprite2D _animatedSprite;
+	CollisionShape2D _collision;
+
+    public override void _Ready()
+    {
+        
+        // Inicializa el AnimatedSprite2D obteniéndolo del nodo
+        _animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		_collision = GetNode<CollisionShape2D>("CollisionShape2D");
+		_currentHealth = _maxHealth;
+    }
+
+    public override void _PhysicsProcess(double delta)
 	{
-		Vector2 velocity = Velocity;
+		Vector2 velocity = new Vector2();
+		int direction = 0;
 
-		// Add the gravity.
-		if (!IsOnFloor())
-		{
-			velocity += GetGravity() * (float)delta;
+		if (Input.IsActionPressed("move_left")){
+			direction -= 1;
+		}
+		if (Input.IsActionPressed("move_right")){
+			direction += 1;
+		}
+		if (direction != 0){
+			velocity.X = Mathf.Lerp(velocity.X, direction * Speed, _acceleration);
+		}else{
+			velocity.X = Mathf.Lerp(velocity.X, 0, _friction);
+		}
+		if (Input.IsActionJustPressed("jump")){
+
+			if (IsOnFloor()){
+				_animatedSprite.Play("jump");
+				velocity.Y -= _jumpHeight;
+			}
 		}
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		{
-			velocity.Y = JumpVelocity;
+		if (Input.IsActionJustPressed("dash")){
+			if (Input.IsActionPressed("move_left")){
+				velocity.X = _dashSpeed;
+			}
+			if (Input.IsActionPressed("move_right")){
+				velocity.X = _dashSpeed;
+			}
 		}
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
-		{
-			velocity.X = direction.X * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		}
-
-		Velocity = velocity;
+		
+		velocity.Y += (float)(_gravity * delta);
+ 		
 		MoveAndSlide();
 	}
 }

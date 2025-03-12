45b50a383fe0ef887e29c203bf9029f1b215ee89
lnarote777 <lnarote777@iesrafaelalberti.es>
2025-03-13 00:34:21 +0100

    muestra listado jugadores pr consola

--- scripts/Winer.cs
+++ scripts/Winer.cs
@@ -1,7 +1,9 @@
 using Godot;
 using Godot.NativeInterop;
 using System;
+using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
 
 public partial class Winer : Control
 {
@@ -39,6 +41,8 @@ public partial class Winer : Control
 	}
 
 	public void _on_ok_pressed(){
+
+		//POST
 		string nombreJugador = nombreTextEdit.Text;
 
 		var jugador = new JugadorData{
@@ -60,19 +64,69 @@ public partial class Winer : Control
 		string[] headers = new string[] { "Content-Type: application/json" };
         httpRequest.Request(url, headers, HttpClient.Method.Post, jsonData);
 
-        // Oculta el Control personalizado después de guardar
         popupNombre.Visible = false;
+		
+	}
+
+	public void _on_get_players_pressed(){
+		Error error = httpRequest.Request(url);
+		if (error != Error.Ok)
+		{
+			GD.Print("Error en la solicitud GET: " + error);
+		}
 	}
 
 	public void _on_http_request_request_completed(long result, long responseCode, string[] headers, byte[] body){
+
+		string responseText = Encoding.UTF8.GetString(body);
+
 		if (responseCode == 201) 
         {
             GD.Print("Jugador guardado exitosamente.");
         }
-        else
+        else if (responseCode == 200){
+		
+			string[] lista = responseText.Split(new string[] { "}," }, StringSplitOptions.RemoveEmptyEntries);
+			
+			if (lista == null){
+				GD.Print("Aun no hay jugadores...");
+			}
+
+			GD.Print("\n===== Lista de Jugadores =====\n");
+
+			foreach (var jugador in lista){
+				string id = ExtraerValor(jugador, "\"id\":");
+				string nombre = ExtraerValor(jugador, "\"name\":");
+				string coins = ExtraerValor(jugador, "\"coins\":");
+				string points = ExtraerValor(jugador, "\"points\":");
+				string connection = ExtraerValor(jugador, "\"ultimaConexion\":");
+
+				GD.Print($"ID: {id}");
+				GD.Print($"	Nombre: {nombre}");
+				GD.Print($"	Monedas: {coins}");
+				GD.Print($"	Puntos: {points}");
+				GD.Print($"	Última conexión: {connection}");
+				GD.Print("--------------------------------");
+			};
+		}
+		else
         {
-            GD.Print("Error al guardar el jugador. Código de respuesta: " + responseCode);
+            GD.Print("Error - Código de respuesta: " + responseCode);
         }
 	}
+
+	string ExtraerValor(string texto, string clave)
+	{
+		int inicio = texto.IndexOf(clave);
+		if (inicio == -1) return "";  
+
+		inicio += clave.Length;  
+		int fin = texto.IndexOf(",", inicio);  
+		if (fin == -1) fin = texto.IndexOf("}", inicio);  
+
+		if (fin == -1) return texto.Substring(inicio).Trim().Trim('"');  
+		return texto.Substring(inicio, fin - inicio).Trim().Trim('"');  
+	}
+
 
 }
